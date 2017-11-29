86bae0f04947fb9bde981f615c4c02444ff1a3c8
TOMITA Masahiro <tommy@tmtm.org>
2017-11-30 00:01:26 +0900

    bundle gem reveal-ck-mermaid-plugin

---
+++ lib/reveal/ck/mermaid/plugin.rb
@@ -0,0 +1,11 @@
+require "reveal/ck/mermaid/plugin/version"
+
+module Reveal
+  module Ck
+    module Mermaid
+      module Plugin
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/reveal/ck/mermaid/plugin/version.rb
@@ -0,0 +1,9 @@
+module Reveal
+  module Ck
+    module Mermaid
+      module Plugin
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
